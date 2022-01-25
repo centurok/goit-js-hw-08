b9259033100ae2704036976c49332338755e91da
Vasil Bilous <centurok@gmail.com>
2022-01-25 12:29:47 +0200

    1 and 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+gallery.addEventListener('click', onPictureClick);
+
+function createGalleryMarkup(pictures) {
+  const markup = pictures
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+  return markup;
+}
+
+function onPictureClick(e) {
+  e.preventDefault();
+}
+let lightBox = new SimpleLightbox('.gallery a', { captionsData: 'alt' }, { captionDelay: 250 });
 
 console.log(galleryItems);
